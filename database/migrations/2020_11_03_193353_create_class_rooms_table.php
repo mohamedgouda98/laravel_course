d732345badd3434b39024cebc8626af9baf922df
Rehab Ragab <rehab.ragab@appssquare.com>
2020-11-03 21:53:09 +0200

    migrations

---
+++ database/migrations/2020_11_03_193353_create_class_rooms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class_rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('school_id')->unsigned();
+            $table->foreign('school_id')->references('id')->on('schools')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('class_rooms');
+    }
+}
